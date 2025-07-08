e633d86e8ac104411cea5498428408a3019295f3
Vinicius Teixeira <vinist.002@gmail.com>
2025-07-08 12:59:35 -0300

    Descrição dos Services

--- src/main/java/one/digitalinnovation/lab_padroes_de_projeto_spring/service/ClienteService.java
+++ src/main/java/one/digitalinnovation/lab_padroes_de_projeto_spring/service/ClienteService.java
@@ -2,6 +2,12 @@ package one.digitalinnovation.lab_padroes_de_projeto_spring.service;
 
 import one.digitalinnovation.lab_padroes_de_projeto_spring.model.Cliente;
 
+/**
+ * Interface que define o padrão Strategy no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ */
+
 public interface ClienteService {
 
     Iterable<Cliente> buscarTodos();
--- src/main/java/one/digitalinnovation/lab_padroes_de_projeto_spring/service/ViaCepService.java
+++ src/main/java/one/digitalinnovation/lab_padroes_de_projeto_spring/service/ViaCepService.java
@@ -5,6 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+/**
+ * Client HTTP, criado via OpenFeign, para o consumo da API do ViaCEP.
+ */
+
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
 
