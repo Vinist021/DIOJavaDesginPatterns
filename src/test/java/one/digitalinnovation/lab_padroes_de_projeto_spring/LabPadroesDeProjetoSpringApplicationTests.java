a1e6876bacad4ab5e4d21c4e03563c62cf62707f
Vinicius Teixeira <vinist.002@gmail.com>
2025-07-08 12:26:27 -0300

    Inicializando o projeto

---
+++ src/test/java/one/digitalinnovation/lab_padroes_de_projeto_spring/LabPadroesDeProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.lab_padroes_de_projeto_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesDeProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
